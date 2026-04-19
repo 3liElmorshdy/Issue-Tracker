f370f154900a3f4334c95bd6ee3c6e082e161d17
3li A7med <alielmorshedy10@gmail.com>
2026-04-19 22:37:18 +0200

    style Active Link in navBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa6";
-
+import classNames from 'classnames';
 const NavBar = () => {
+
+  const currentPath = usePathname();
+  console.log(currentPath);
   const links = [
     {
       label: "Dashboard",
@@ -23,7 +28,12 @@ const NavBar = () => {
     {links.map((link) => (
       <li key={link.href} className='space-x-6'>
         <Link
-          className="text-zinc-500 hover:text-zinc-800 transition-colors "
+        className={classNames(
+          'text-zinc-500 hover:text-zinc-800 transition-colors',
+          {
+            'text-zinc-800': currentPath === link.href
+          }
+        )}
           href={link.href}
         > 
           {link.label}
